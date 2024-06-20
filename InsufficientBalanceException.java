8007383b757bfb175f84ff1af723561faab980ab
Tanneti Sri Surya Durga Prasad <115442804+Tannetisrisuryadurgaprasad@users.noreply.github.com>
2024-06-20 21:11:30 +0530

    Add files via upload

---
+++ InsufficientBalanceException.java
@@ -0,0 +1,16 @@
+
+package exception;
+
+public class InsufficientBalanceException extends Exception 
+{
+	public InsufficientBalanceException()
+	{
+		System.out.println("Low Balance");
+	}
+	
+    public InsufficientBalanceException(String msg)
+    {
+    	
+    		System.out.println(msg);
+    }
+}
